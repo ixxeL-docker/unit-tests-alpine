837515ecf026c0d2b65888d0b4a5e977fe1f951b
Frédéric SPIERS <fredspiers@gmail.com>
2022-02-26 16:22:23 +0100

    Create get-gitlab-project-id.py

---
+++ resources/get-gitlab-project-id.py
@@ -0,0 +1,18 @@
+import gitlab
+import os
+from envparse import env
+
+GITLAB_URL = env('GITLAB_URL')
+GITLAB_TOKEN = env('GITLAB_TOKEN')
+GITLAB_PROJECT = env('GITLAB_PROJECT')
+
+gl = gitlab.Gitlab(GITLAB_URL, GITLAB_TOKEN)
+
+def get_id():
+    project = gl.projects.get(GITLAB_PROJECT)
+    print(project.id)
+
+def main():
+    get_id()
+
+main()
